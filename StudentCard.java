b5ee7cda7ecd305c09a163ff9181ab56ea45919f
Hoei Mizuhara <h141862.tut.ac.jp>
2016-12-16 17:49:58 +0900

    2 commit

--- StudentCard.java
+++ StudentCard.java
@@ -1,10 +1,15 @@
 package ICCharger;
+import java.util.ArrayList;
 
 public class StudentCard {
 	private String studentName;
 	private int studentNum;
 	private int accountBalance;//‹à
+	private ArrayList<StudentCard> studentCardList_;
 	
+	StudentCard(){
+		studentCardList_.add(this);
+	}
 	
 	int get_money(){
 		return accountBalance;
